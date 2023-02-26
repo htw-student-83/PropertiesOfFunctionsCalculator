9adfc3c755f180f75a340fad318af9bb2d6902b8
Daniel Granaß <student2683@web.de>
2023-02-26 15:55:14 +0100

    Create a new project with a testclass.

---
+++ src/UI.java
@@ -0,0 +1,6 @@
+public class UI {
+    public static void main(String[]args){
+        //TODO Benutzerschnittstelle
+        //Nimmt die Befehl vom User entgegen und leitet diese an die GL weiter
+    }
+}
---
+++ test/businesslogic/calculationsTest.java
@@ -0,0 +1,30 @@
+package businesslogic;
+
+import org.testng.annotations.Test;
+
+class calculationsTest {
+
+    @Test
+    void calculateTheIntersectionWithXaxis() {
+    }
+
+    @Test
+    void calculateTheIntersectionOfFunctions() {
+    }
+
+    @Test
+    void calculateTheGradient() {
+    }
+
+    @Test
+    void calculateTheAngle() {
+    }
+
+    @Test
+    void calculateTheParallelism() {
+    }
+
+    @Test
+    void calculateTheIntersectionWithYaxis() {
+    }
+}
